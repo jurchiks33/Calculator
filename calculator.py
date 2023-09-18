f6caedad522d5289b312bb4a1497cbdd68d3e414
jurchiks33 <jurisslisans@gmail.com>
2023-09-18 12:34:10 +0200

    Introduced additional functions

--- calculator.py
+++ calculator.py
@@ -1,10 +1,23 @@
 import tkinter as tk
+import math
 
 def evaluate(event):
     try:
-        result.set(eval(entry.get()))
+        expr = entry.get()
+
+        if "^2" in expr:
+            result.set(float(expr.split("^2")[0]) ** 2)
+        elif "√" in expr:
+            result.set(math.sqrt(float(expr.split("√")[1])))
+        elif "1/" in expr:
+            result.set(1 / float(expr.split("1/")[1]))
+        elif "!" in expr:
+            result.set(math.factorial(int(expr.split("!")[0])))
+        else:
+            result.set(eval(expr))
     except Exception as e:
         result.set("Error")
+
 
 def clear(event):
     entry.delete(0, tk.END)
@@ -28,12 +41,14 @@ buttons = [
     '7', '8', '9', '/',
     '4', '5', '6', '*',
     '1', '2', '3', '-',
+    '^2', '√', '1/', '!',
     'C', '0', '=', '+'
 ]
 
 row, col = 0, 0
 for btn in buttons:
-    button_command = lambda b=btn: entry.insert(tk.END, b if b != "C" else "")
+    button_command = (lambda b=btn: 
+                      lambda: entry.insert(tk.END, b if b != "C" else ""))()
     tk.Button(button_frame, text=btn, width=5, height=2, command=button_command).grid(row=row, column=col)
     col += 1
     if col > 3:
@@ -52,10 +67,10 @@ color_dropdown.pack(pady=10)
 entry.pack(pady=20)
 result_label.pack(pady=20)
 
-clear_button = button_frame.winfo_children()[12]
+clear_button = button_frame.winfo_children()[16]
 clear_button.bind('<Button-1>', clear)
 
-equal_button = button_frame.winfo_children()[14]
+equal_button = button_frame.winfo_children()[18]
 equal_button.bind('<Button-1>', evaluate)
 
 app.mainloop()
