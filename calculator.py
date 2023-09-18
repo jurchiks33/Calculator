597c8e1d6e7070b089aa0511fe02ff652fce7994
jurchiks33 <jurisslisans@gmail.com>
2023-09-18 11:03:44 +0200

    More operations for calculator

--- calculator.py
+++ calculator.py
@@ -1,22 +1,49 @@
 import tkinter as tk
 
-def on_calculate():
+def evaluate(event):
     try:
-        result = eval(entry.get())
-        label.config(text=f"Result: {result}")
+        result.set(eval(entry.get()))
     except Exception as e:
-        label.config(text=f"Error: {str(e)}")
+        result.set("Error")
+
+def clear(event):
+    entry.delete(0, tk.END)
+    result.set("")
 
 app = tk.Tk()
-app.title("Basic Calculator")
+app.title("Enhanced Calculator")
 
 entry = tk.Entry(app, width=40)
+entry.bind("<Return>", evaluate)
+
+button_frame = tk.Frame(app)
+button_frame.pack(pady=10)
+
+buttons = [
+    '7', '8', '9', '/',
+    '4', '5', '6', '*',
+    '1', '2', '3', '-',
+    'C', '0', '=', '+'
+]
+
+row, col = 0, 0
+for button in buttons:
+    tk.Button(button_frame, text=button, width=5, height=2, command=lambda b=button: entry.insert(tk.END, b if b != "C" else "")).grid(row=row, column=col)
+    col += 1
+    if col > 3:
+        col = 0
+        row += 1
+
+result = tk.StringVar()
+result_label = tk.Label(app, textvariable=result, font=("Arial", 18))
+
 entry.pack(pady=20)
+result_label.pack(pady=20)
 
-calculate_button = tk.Button(app, text="Calculate", command=on_calculate)
-calculate_button.pack(pady=10)
+clear_button = button_frame.winfo_children()[12]
+clear_button.bind('<Button-1>', clear)
 
-label = tk.Label(app, text="Result: ")
-label.pack(pady=20)
+equal_button = button_frame.winfo_children()[14]
+equal_button.bind('<Button-1>', evaluate)
 
 app.mainloop()
